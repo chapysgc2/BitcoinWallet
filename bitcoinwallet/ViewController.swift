cbc10b5fa022073575a8ecd494402a4efa295812
hazelgc2 <hazelo235@gmail.com>
2024-12-17 21:18:13 -0600

    Initial Commit

---
+++ bitcoinwallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  bitcoinwallet
+//
+//  Created by Hazel Alain on 17/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ bitcoinwalletTests/bitcoinwalletTests.swift
@@ -0,0 +1,17 @@
+//
+//  bitcoinwalletTests.swift
+//  bitcoinwalletTests
+//
+//  Created by Hazel Alain on 17/12/24.
+//
+
+import Testing
+@testable import bitcoinwallet
+
+struct bitcoinwalletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
